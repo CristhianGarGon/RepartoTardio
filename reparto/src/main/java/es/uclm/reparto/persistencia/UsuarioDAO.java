2517464e4e08ccaea28bf6b211f39582535e1584
Rober192004 <Rober@LAPTOP-VIKRGMMB>
2024-11-20 20:34:54 +0100

    cambios base de datos

--- reparto/src/main/java/es/uclm/reparto/persistencia/UsuarioDAO.java
+++ reparto/src/main/java/es/uclm/reparto/persistencia/UsuarioDAO.java
@@ -1,7 +1,7 @@
 package es.uclm.reparto.persistencia;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import es.uclm.Reparto.business.entity.Usuario;
+import es.uclm.reparto.entidades.Usuario;
 
 public interface UsuarioDAO extends JpaRepository<Usuario, Long> {
   
